e6f912b9e292ef0d1a8a4ce5309cd03d387aa165
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 14:01:21 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,8 @@
 import pytest
-import src.exercise
+from src.exercise import sum
 
-def test_exercise():
-    #implement tests here
-    assert 0 == 0
+def test_exercise(capsys):
+    numbers = [1,3,10,-1,5,0]
+    sum(numbers)
+    out, err = capsys.readouterr()
+    assert out == 18
